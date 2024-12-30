4efccbd01567fd2e5a74413b42e4896944192068
SeanC <G00402034@atu.ie>
2024-12-30 15:56:22 +0000

    added new class order for storing order info

--- src/main/java/ie/atu/delivery_order_service/Order.java
+++ src/main/java/ie/atu/delivery_order_service/Order.java
@@ -1,4 +1,23 @@
 package ie.atu.delivery_order_service;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "orders" )
+@Data
 public class Order {
+    @Id
+    private String orderId;
+    private String customerId;
+    private String restaurantId;
+    private List<String> items;
+    private double totalAmount;
+    private String deliveryAddress;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
---
+++ src/test/java/ie/atu/delivery_order_service/DeliveryOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.delivery_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
